a3531e0d5dd517dfed3a5b3906e586d6a7cecc73
Milen Atanasov <168510343+Milen03@users.noreply.github.com>
2026-04-03 20:04:29 +0300

    Add files via upload

---
+++ Asynchronous-Programing/asyng-funcion.js
@@ -0,0 +1,35 @@
+function propose() {
+    const promise = new Promise((resolve, reject) => {
+        console.log('Will you marry me?');
+
+        setTimeout(() => {
+            if (Math.random() < 0.5) {
+                resolve('Just married!');
+            } else {
+                reject('It\'s not you, it\s me!');
+            }
+        }, 3650);
+    });
+
+    return promise;
+}
+
+async function gropePropagal() {
+     console.log('Before proposal');
+     try{
+        const result = await propose()
+
+        console.log(result);
+        console.log('After proposal');
+        
+     }catch(err){
+        console.log(err);
+        
+     }
+     
+}
+
+console.log('Before group proposal');
+gropePropagal()
+console.log('After group proposal');
+
